882e1acb9e43445e50d58b2da7819eda5b8213e0
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-01-15 00:24:03 +0530

    Added word count feature

--- cmd/wc/wc.go
+++ cmd/wc/wc.go
@@ -20,12 +20,15 @@ func wc(option string, file string) (string, error) {
 		return getByteCount(file)
 	} else if option == "-l" {
 		return getLineCount(file)
+	} else if option == "-w" {
+		return getWordCount(file)
 	}
 	err := fmt.Sprintf("Invalid option")
 	return "", errors.New(err)
 }
 
 func getByteCount(file string) (string, error) {
+	// Read the file
 	fd, err := ioutil.ReadFile(file)
 	if err != nil {
 		return "", err
@@ -46,5 +49,26 @@ func getLineCount(file string) (string, error) {
 	}
 
 	result := fmt.Sprintf("%d %s", lineCount, file)
+	return result, nil
+}
+
+func getWordCount(file string) (string, error) {
+	fd, err := os.Open(file)
+	if err != nil {
+		return "", err
+	}
+	scanner := bufio.NewScanner(fd)
+
+	scanner.Split(bufio.ScanWords)
+
+	countWords := 0
+	for scanner.Scan() {
+		countWords++
+	}
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	result := fmt.Sprintf("%d %s", countWords, file)
 	return result, nil
 }
--- cmd/wc/wc_test.go
+++ cmd/wc/wc_test.go
@@ -10,8 +10,8 @@ func TestNumberOfBytes(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	want := "342190 test.txt"
 
+	want := "342190 test.txt"
 	if got != want {
 		t.Error("Strings don't match")
 	}
@@ -22,11 +22,37 @@ func TestNumberOfLines(t *testing.T) {
 	got, err := wc("-l", fileName)
 	if err != nil {
 		t.Error(err)
-		t.FailNow()
 	}
+
 	want := "7145 test.txt"
 
 	if got != want {
 		t.Error("String don't match")
+	}
+}
+
+func TestNumberOfWords(t *testing.T) {
+	fileName := "test.txt"
+	got, err := wc("-w", fileName)
+	if err != nil {
+		t.Error(err)
+	}
+
+	want := "58164 test.txt"
+	if got != want {
+		t.Error("String don't match")
+	}
+}
+
+func TestNumberOfCharacters(t *testing.T) {
+	fileName := "test.txt"
+	got, err := wc("-m", fileName)
+	if err != nil {
+		t.Error(err)
+	}
+
+	want := "339292 test.txt"
+	if got != want {
+		t.Error("Strings don't match")
 	}
 }
